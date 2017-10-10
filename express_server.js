aade87ccc34ebfe80c734f572f2da24f402fb881
gurpm1991 <gurpm91@gmail.com>
2017-10-10 20:06:55 +0000

    updates

--- express_server.js
+++ express_server.js
@@ -4,10 +4,13 @@ var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {longurl: "http://www.lighthouselabs.ca"},
+  "9sm5xK": {longurl: "http://www.google.com"},
 };
 
 app.get("/", (req, res) => {
@@ -26,6 +29,15 @@ app.get("/urls", (req, res) => {  //unsure if this line is correct
 app.get("/urls/:id", (req, res) => { //unsure if this function is correct
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
